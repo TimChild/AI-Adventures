6c31c6755c201db4a4bdecb91dc51cc469f04272
Tim Child <timjchild@phas.ubc.ca>
2023-05-23 10:39:00 -0700

    Updated to make MVP

--- langchain_test/app.py
+++ langchain_test/app.py
@@ -10,41 +10,15 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.chains import ConversationalRetrievalChain
 
+import os
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-
-
-app.layout = dbc.Container(
-    dbc.Row(
-        dbc.Col(
-            [
-                html.H1("Igor Doc Bot", className="text-center"),
-                dbc.Form(
-                    [
-                        dbc.Row(
-                            [
-                                dbc.Col(dbc.Input(id="input-box", placeholder="Enter text...")),
-                                dbc.Col(dbc.Button("Submit", id="submit-button", color="primary")),
-                            ],
-                        ),
-                    ],
-                    className="mt-3",
-                ),
-                dcc.Markdown(id="markdown-cell"),
-            ],
-            width=6,
-        ),
-        justify="center",
-    ),
-    className="mt-5",
-)
-
+os.environ['OPENAI_API_KEY'] = "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"
 
 embedding = OpenAIEmbeddings()
 db = FAISS.load_local("faiss_dbs", embeddings=embedding, index_name="igor-test")
 
 # TODO: The chat history should be stored in a ServerSide store using the dash-extensions package
-qa = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0.0), db.as_retriever())
+qa = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0.0), db.as_retriever(k=3))
 chat_history = []
 
 
@@ -61,27 +35,68 @@ def make_markdown(history, answer):
     return markdown_string
 
 
+# Build the app
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], url_base_pathname='/igor-doc-bot/')
+
+
+history_store = dcc.Store(id='store-history', data=[])
+
+app.layout = dbc.Container(
+    dbc.Row(
+        dbc.Col(
+            [
+                html.H1("Igor Doc Bot", className="text-center"),
+                html.H4("Ask me anything where you would expect the answer to be in the Igor .ihf files", className="text-center"),
+                dbc.Form(
+                    [
+                        dbc.Row(
+                            [
+                                # dbc.Col(dbc.Input(id="input-box", placeholder="E.g. How can I display a 2D wave? (include an example)")),
+                                dbc.Col(dcc.Textarea(id="input-box", placeholder="E.g. How can I display a 2D wave? (include an example)", style={'width': '100%', 'height': 100})),
+                                dbc.Col(dbc.Button("Submit", id="submit-button", color="primary")),
+                            ],
+                        ),
+                    ],
+                    className="mt-3",
+                ),
+                dcc.Markdown(id="markdown-cell"),
+                history_store,
+            ],
+            width=6,
+        ),
+        justify="center",
+    ),
+    className="mt-5",
+)
+
+
 @app.callback(
     Output('markdown-cell', 'children'),
+    Output('store-history', 'data'),
     Input('submit-button', 'n_clicks'),
     State('input-box', 'value'),
+    State('store-history', 'data'),
 )
-def update_markdown(n_clicks, query):
+def update_markdown(n_clicks, query, chat_history):
+    chat_history = [tuple(hist) for hist in chat_history]
     if n_clicks:
         result = qa({'question': query, 'chat_history': chat_history})
         chat_history.append((query,  result['answer']))
         md = make_markdown(chat_history, result['answer'])
-        return md
+        return md, chat_history
 
-    return ""
-
-
-
-
+    return "", chat_history
 
 
 if __name__ == "__main__":
+    # app.run(
+    #     port=8085,
+    #     debug=True,
+    #     dev_tools_hot_reload=True,
+    # )
     app.run(
-        debug=True,
-        dev_tools_hot_reload=True,
+        host='0.0.0.0',
+        port=8080,
+        debug=False,
+        dev_tools_hot_reload=False,
     )
