becc7bb0722971f35ae6e7521f7d8e523d3508d0
Tim Child <timjchild@phas.ubc.ca>
2023-06-20 20:12:04 -0700

    black format

--- igor_doc_bot/igor_funciton_loading.py
+++ igor_doc_bot/igor_funciton_loading.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
+from pathlib import Path
 from dataclasses import dataclass, asdict
 from typing import List
 import json
+from tqdm.auto import tqdm
 from IPython.display import Markdown, display
 from langchain.schema import Document
 
@@ -52,7 +54,7 @@ from langchain.schema import Document
 #         stripped_line (str): The line of text with leading/trailing whitespace removed.
 
 #     Returns:
-#         tuple: A tuple containing a `Function` dataclass instance representing the new function, 
+#         tuple: A tuple containing a `Function` dataclass instance representing the new function,
 #                and a list to accumulate the function's docstring.
 #     """
 #     return function_data, function_data.docstring
@@ -68,7 +70,7 @@ from langchain.schema import Document
 
 #     Returns:
 #         List[str]: The updated docstring list.
-#     """    
+#     """
 #     docstring.append(stripped_line[2:].strip())
 #     return docstring
 
@@ -105,7 +107,7 @@ class IgorObject:
     @property
     def metadata(self):
         data = asdict(self)
-        data.pop('code')
+        data.pop("code")
         return data
 
     def to_markdown(self, include_code: bool = True) -> str:
@@ -118,15 +120,15 @@ class IgorObject:
         Returns:
             str: The markdown string representation of the IgorObject.
         """
-        summary = f'## Summary\n\n'
+        summary = f"## Summary\n\n"
         for key, value in self.metadata.items():
-            if key != 'docstring':
-                summary += f'- **{key.capitalize()}**: {value}\n'
-        
-        docstring = f'## Docstring\n\n```\n{self.docstring}\n```\n'
-        
+            if key != "docstring":
+                summary += f"- **{key.capitalize()}**: {value}\n"
+
+        docstring = f"## Docstring\n\n```\n{self.docstring}\n```\n"
+
         if include_code:
-            code = f'## Code\n\n```igor\n{self.code}\n```\n'
+            code = f"## Code\n\n```igor\n{self.code}\n```\n"
             md = summary + docstring + code
         else:
             md = summary + docstring
@@ -154,8 +156,7 @@ class IgorObject:
         """
         igor_object = cls(code=document.page_content, **document.metadata)
         return igor_object
-    
-    
+
     def to_json(self) -> dict:
         """
         Converts the IgorObject to a JSON-compatible dictionary.
@@ -164,7 +165,7 @@ class IgorObject:
             dict: The dictionary representing the IgorObject.
         """
         return asdict(self)
-    
+
     @classmethod
     def from_json(cls, json_dict: dict | str) -> IgorObject:
         """
@@ -177,7 +178,7 @@ class IgorObject:
             IgorObject: The `IgorObject` dataclass instance representing the dictionary or file.
         """
         if isinstance(json_dict, str):
-            with open(json_dict, 'r') as f:
+            with open(json_dict, "r") as f:
                 json_dict = json.load(f)
         return cls(**json_dict)
 
@@ -192,15 +193,15 @@ class IgorFile:
     functions: List[Function]
     structures: List[Structure]
     macros: List[Macro]
-        
+
     def to_file(self, filename: str) -> None:
         """
         Writes the IgorFile to a JSON file.
 
         Args:
             filename (str): The name of the file to write to.
-        """        
-        with open(filename, 'w') as f:
+        """
+        with open(filename, "w") as f:
             json.dump(asdict(self), f)
 
     @classmethod
@@ -214,19 +215,21 @@ class IgorFile:
         Returns:
             IgorFile: The `IgorFile` dataclass instance representing the file.
         """
-        with open(filename, 'r') as f:
+        with open(filename, "r") as f:
             data = json.load(f)
 
         # Convert dicts back to dataclass instances
-        data['functions'] = [Function.from_json(func) for func in data['functions']]
-        data['structures'] = [Structure.from_json(struct) for struct in data['structures']]
-        data['macros'] = [Macro.from_json(macro) for macro in data['macros']]
+        data["functions"] = [Function.from_json(func) for func in data["functions"]]
+        data["structures"] = [
+            Structure.from_json(struct) for struct in data["structures"]
+        ]
+        data["macros"] = [Macro.from_json(macro) for macro in data["macros"]]
         return cls(**data)
 
 
-
-
-def start_new_igor_object(line_number: int, line: str, stripped_line: str) -> Tuple[IgorObject, str]:
+def start_new_igor_object(
+    line_number: int, line: str, stripped_line: str
+) -> tuple[IgorObject, str]:
     """
     Starts processing a new Igor object from the file.
 
@@ -236,25 +239,33 @@ def start_new_igor_object(line_number: int, line: str, stripped_line: str) -> Tu
         stripped_line (str): The line of text with leading/trailing whitespace removed.
 
     Returns:
-        tuple: A tuple containing an `IgorObject` dataclass instance representing the new object, 
+        tuple: A tuple containing an `IgorObject` dataclass instance representing the new object,
                and a string indicating the type of the object (either 'function', 'structure', or 'macro').
     """
-    name = stripped_line.split(' ')[1].split('(')[0]
-    declaration = ''.join(stripped_line.split(' ')[1:])
+    name = stripped_line.split(" ")[1].split("(")[0]
+    declaration = "".join(stripped_line.split(" ")[1:])
     stripped_line = stripped_line.lower()
-    if stripped_line.startswith('function'):
+    if stripped_line.startswith("function"):
         NewObject = Function
-        obj_type = 'function'
-    elif stripped_line.startswith('structure'):
+        obj_type = "function"
+    elif stripped_line.startswith("structure"):
         NewObject = Structure
-        obj_type = 'structure'
-    elif stripped_line.startswith('window'):
+        obj_type = "structure"
+    elif stripped_line.startswith("window"):
         NewObject = Macro
-        obj_type = 'macro'
+        obj_type = "macro"
     else:
         raise RuntimeError(f"Don't know how to handle: {stripped_line}")
 
-    igor_object_data = NewObject(name=name, declaration=declaration, start_line=line_number, code=[], docstring=[], end_line=None, filename=None)
+    igor_object_data = NewObject(
+        name=name,
+        declaration=declaration,
+        start_line=line_number,
+        code=[],
+        docstring=[],
+        end_line=None,
+        filename=None,
+    )
     return igor_object_data, obj_type
 
 
@@ -270,7 +281,9 @@ def accumulate_docstring(igor_object: IgorObject, line: str) -> None:
     igor_object.code.append(line)
 
 
-def end_current_igor_object(igor_object_data: IgorObject, end_line: int, filename: str) -> IgorObject:
+def end_current_igor_object(
+    igor_object_data: IgorObject, end_line: int, filename: str
+) -> IgorObject:
     """
     Finalizes processing of an Igor object.
 
@@ -285,9 +298,9 @@ def end_current_igor_object(igor_object_data: IgorObject, end_line: int, filenam
     igor_object_data.end_line = end_line
     igor_object_data.filename = filename
     if igor_object_data.docstring:
-        igor_object_data.docstring = '\n'.join(igor_object_data.docstring)
+        igor_object_data.docstring = "\n".join(igor_object_data.docstring)
     if igor_object_data.code:
-        igor_object_data.code = ''.join(igor_object_data.code)
+        igor_object_data.code = "".join(igor_object_data.code)
     return igor_object_data
 
 
@@ -297,13 +310,13 @@ def parse_igor_file(filename: str, verbose: bool = False) -> IgorFile:
 
     Args:
         filename (str): The name of the file to parse.
-        verbose (bool, optional): If True, print out additional information during the parsing process. 
+        verbose (bool, optional): If True, print out additional information during the parsing process.
                                    Defaults to False.
 
     Returns:
         IgorFile: An `IgorFile` dataclass instance representing the parsed file.
     """
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         lines = f.readlines()
 
     preamble = []
@@ -317,23 +330,34 @@ def parse_igor_file(filename: str, verbose: bool = False) -> IgorFile:
     for i, line in enumerate(lines, start=1):
         stripped = line.strip()
 
-        if stripped.lower().startswith(('function', 'structure', 'window')):
+        # In case there is a "threadsafe", remove from the stripped string that is used
+        # for identification
+        if stripped.lower().startswith("threadsafe"):
+            stripped = stripped[10:].strip()
+
+        if stripped.lower().startswith(("function", "structure", "window")):
             igor_object_data, object_type = start_new_igor_object(i, line, stripped)
             if verbose:
-                print(f'Started adding {igor_object_data.name}')
-            if object_type == 'function':
+                print(f"Started adding {igor_object_data.name}")
+            if object_type == "function":
                 igor_object_list = functions
-            elif object_type == 'structure':
+            elif object_type == "structure":
                 igor_object_list = structures
-            elif object_type == 'macro':
+            elif object_type == "macro":
                 igor_object_list = macros
-        elif igor_object_data and stripped.startswith('//') and (len(igor_object_data.code) == len(igor_object_data.docstring)):
+        elif (
+            igor_object_data
+            and stripped.startswith("//")
+            and (len(igor_object_data.code) - 1 == len(igor_object_data.docstring))
+        ):
             accumulate_docstring(igor_object_data, line)
-        elif stripped.lower() in ('end', 'endstructure', 'endmacro'):
+        elif stripped.lower() in ("end", "endstructure", "endmacro"):
             if verbose:
-                print(f'Finished adding {igor_object_data.name}')
-            igor_object_list.append(end_current_igor_object(igor_object_data, i, filename))
-            igor_object_data, igor_object_list, docstring =  None, None, None
+                print(f"Finished adding {igor_object_data.name}")
+            igor_object_list.append(
+                end_current_igor_object(igor_object_data, i, filename)
+            )
+            igor_object_data, igor_object_list, docstring = None, None, None
         elif igor_object_data:  # We are inside an Igor object block
             igor_object_data.code.append(line)
         elif all([len(l) == 0 for l in [functions, structures, macros]]):
@@ -341,6 +365,15 @@ def parse_igor_file(filename: str, verbose: bool = False) -> IgorFile:
         else:
             if verbose:
                 if line:
-                    print(f'Ignoring line: {line}')
+                    print(f"Ignoring line: {line}")
 
-    return IgorFile(filename=filename, preamble=''.join(preamble), functions=functions, structures=structures, macros=macros)
+    return IgorFile(
+        filename=filename,
+        preamble="".join(preamble),
+        functions=functions,
+        structures=structures,
+        macros=macros,
+    )
+
+
+def parse_all_ipf_files(
